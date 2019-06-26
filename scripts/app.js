b6a677fc7814063c4232bb4d456e9b21b314de56
MattVinall <matt.vinall7@gmail.com>
2019-06-25 23:50:19 -0400

    finished styling blog section using grid layout, general styling

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,9 @@
 // DOC READY
 $(function() {
-	// Hamburger Menu
-	$('.hamburger').on('click', () => {
-		$('.navigation-container').fadeIn(300);
-	});
-	$('.exit-responsive').on('click', () => {
-		$('.navigation-container').fadeOut(300);
-	});
-	$('a').on('click', () => {
-		$('.navigation-container').fadeOut(300);
-	});
-
 	// flickity carousel
 	$('.carousel').flickity({
-		wrapAround: true
+		wrapAround: true,
+		dots: true
 	});
 
 	// masonry.js
