18848b58eb3f356a07d3fe814bbde7b75ac747e5
MattVinall <matt.vinall7@gmail.com>
2019-06-26 13:22:32 -0400

    added mediaQueries partial, got carousel working with slick.js, modified app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,16 @@
 // DOC READY
 $(function() {
-	// flickity carousel
-	$('.carousel').flickity({
-		wrapAround: true,
-		dots: true
-	});
-
 	// masonry.js
 	$('.grid').masonry({
 		itemSelector: '.grid__item',
 		percentagePosition: true
+	});
+
+	// slick.js
+	$('.carousel').slick({
+		slidesToShow: 3,
+		slidesToScroll: 2,
+		autoplay: true,
+		autoplaySpeed: 3000
 	});
 });
